0c2b0b2f0c5e5a78e0f1a44a8f7e56d494cfb1e6
HUY_FA <nguyenvanhuy178tiengiang@gmail.com>
2023-03-19 04:08:45 +0700

    config render third

--- src/tmdb/tmdb.config.js
+++ src/tmdb/tmdb.config.js
@@ -1,5 +1,5 @@
-const baseUrl = process.env.TMDB_BASE_URL;
-const key = process.env.TMDB_KEY;
+const baseUrl = process.env.TMDB_BASE_URL || "https://api.themoviedb.org/3/";
+const key = process.env.TMDB_KEY || '238428fde048a6a969a40597b151ba77';
 
 const getUrl = (endpoint, params) => {
   const qs = new URLSearchParams(params);
