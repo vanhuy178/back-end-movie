833cd272d57b850b98a17948aa81fe83633333e1
HUY_FA <nguyenvanhuy178tiengiang@gmail.com>
2023-03-19 03:33:35 +0700

    config render

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
-  console.log("Mongodb connected");
+  console.log("Mongodb connected ok");
   server.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
   });
